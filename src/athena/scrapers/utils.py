cb38f1aa79cc191fa52ba99d75683e9083e4b40e
8lb6ozbabyjsus <walkerdalton@yahoo.com>
2024-07-31 02:13:00 -0700

    Update utils.py

--- src/athena/scrapers/utils.py
+++ src/athena/scrapers/utils.py
@@ -12,7 +12,7 @@ from requests.adapters import HTTPAdapter, Retry
 
 from ..jobs import CompensationInterval, JobType
 
-logger = logging.getLogger("JobSpy")
+logger = logging.getLogger("Athena")
 logger.propagate = False
 if not logger.handlers:
     logger.setLevel(logging.INFO)
