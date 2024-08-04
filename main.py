4522afda60bf0e318238f22e1c9def0c8e012838
8lb6ozbabyjsus <walkerdalton@yahoo.com>
2024-08-04 14:05:51 -0700

    Small updates

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-def get_directory_structure(rootdir):
-    rootdir = Path(rootdir)
-    dir_structure = []
-    for path in sorted(rootdir.rglob('*')):
-        dir_structure.append(str(path))
-    return dir_structure
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# def get_directory_structure(rootdir):
+#     rootdir = Path(rootdir)
+#     dir_structure = []
+#     for path in sorted(rootdir.rglob('*')):
+#         dir_structure.append(str(path))
+#     return dir_structure
 
 @app.get("/files")
 def read_files():
-    files = get_directory_structure(".")
-    return JSONResponse(content=files)
+    return JSONResponse(200) 
+    # files = get_directory_structure(".")
+    # return JSONResponse(content=files)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
